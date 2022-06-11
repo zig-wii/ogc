14a49c081da4dca47c514461f12c27b8707705f0
Knarkzel <knarkzel@gmail.com>
2022-06-11 14:29:52 +0200

    Bootloader-like function

--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,9 @@ pub const utils = @import("utils.zig");
 pub const Pad = @import("Pad.zig");
 pub const Video = @import("Video.zig");
 pub const Rectangle = @import("Rectangle.zig");
+
+pub fn start(function: fn (*Video) anyerror!void) void {
+    Pad.init();
+    var video = Video.init();
+    function(&video) catch |err| @panic(@errorName(err));
+}

20cf668a12bbffd2171e52225ee97428b008342f
Jae B <doogie1012@gmail.com>
2023-06-21 11:03:30 +1000

    update build to work with 0.11.0-dev.3394+c842deea7

--- build.zig
+++ build.zig
@@ -15,34 +15,45 @@ pub const Options = struct {
 };
 
 pub fn target_wii(builder: *std.build.Builder, comptime options: Options) !*std.build.LibExeObjStep {
+    var build_path: []const u8 = "";
+    if (builder.build_root.path) |new_path| {
+        build_path = new_path;
+    } else {
+        @panic("unable to get build path");
+    }
+
     // ensure devkitpro is installed
-    const devkitpro = try print(builder.allocator, "{s}/zig-devkitpro", .{builder.build_root});
-    const base_folder = try std.fs.openDirAbsolute(builder.build_root, .{});
+    const devkitpro = try print(builder.allocator, "{s}/zig-devkitpro", .{build_path});
+    const base_folder = try std.fs.openDirAbsolute(build_path, .{});
     base_folder.access("zig-devkitpro", .{}) catch |err| if (err == error.FileNotFound) {
         const repository = switch (builtin.target.os.tag) {
             .macos => "https://github.com/zig-wii/devkitpro-mac",
             .windows => "https://github.com/zig-wii/devkitpro-windows",
             else => "https://github.com/zig-wii/devkitpro-linux",
         };
-        try command(builder.allocator, builder.build_root, &.{ "git", "clone", repository, devkitpro });
+        try command(builder.allocator, build_path, &.{ "git", "clone", repository, devkitpro });
     };
     const ext = if (builtin.target.os.tag == .windows) ".exe" else "";
 
     // set build options
-    const mode = builder.standardReleaseOptions();
-    const obj = builder.addObject(options.name, options.root_src);
-    obj.setOutputDir("zig-out");
-    obj.linkLibC();
-    obj.setLibCFile(std.build.FileSource{ .path = cwd() ++ "/libc.txt" });
-    obj.addIncludeDir(try print(builder.allocator, "{s}/libogc/include", .{devkitpro}));
-    obj.setTarget(.{
-        .cpu_arch = .powerpc,
-        .os_tag = .freestanding,
-        .abi = .eabi,
-        .cpu_model = .{ .explicit = &std.Target.powerpc.cpu.@"750" },
-        .cpu_features_add = std.Target.powerpc.featureSet(&.{.hard_float}),
+    const obj = builder.addObject(.{
+        .name = options.name,
+        .root_source_file = .{
+            .path = options.root_src,
+        },
+        .optimize = builder.standardOptimizeOption(.{}),
+        .target = .{
+            .cpu_arch = .powerpc,
+            .os_tag = .freestanding,
+            .abi = .eabi,
+            .cpu_model = .{ .explicit = &std.Target.powerpc.cpu.@"750" },
+            .cpu_features_add = std.Target.powerpc.featureSet(&.{.hard_float}),
+        },
+        .link_libc = true,
     });
-    obj.setBuildMode(mode);
+    obj.setLibCFile(std.build.FileSource{ .path = comptime cwd() ++ "/libc.txt" });
+    obj.addIncludePath(try print(builder.allocator, "{s}/libogc/include", .{devkitpro}));
+    obj.addIncludePath(try print(builder.allocator, "{s}/devkitPPC/powerpc-eabi/include", .{devkitpro}));
 
     // ensure images in textures are converted to tpl
     if (options.textures) |textures| {
@@ -52,7 +63,7 @@ pub fn target_wii(builder: *std.build.Builder, comptime options: Options) !*std.
             if (std.mem.endsWith(u8, entry.name, ".png")) {
                 const input = try print(builder.allocator, "{s}/{s}", .{ textures, entry.name });
                 const conv = try print(builder.allocator, "{s}/tools/bin/gxtexconv" ++ ext, .{devkitpro});
-                try command(builder.allocator, builder.build_root, &.{ conv, "-i", input });
+                try command(builder.allocator, build_path, &.{ conv, "-i", input });
             }
         }
     }
@@ -62,7 +73,9 @@ pub fn target_wii(builder: *std.build.Builder, comptime options: Options) !*std.
     const gcc = try print(builder.allocator, "{s}/devkitPPC/bin/powerpc-eabi-gcc" ++ ext, .{devkitpro});
     const libogc = try print(builder.allocator, "-L{s}/libogc/lib/wii", .{devkitpro});
     const elf2dol = try print(builder.allocator, "{s}/tools/bin/elf2dol" ++ ext, .{devkitpro});
-    const elf = builder.addSystemCommand(&(.{ gcc, "zig-out/" ++ options.name ++ ".o", "-g", "-DGEKKO", "-mrvl", "-mcpu=750", "-meabi", "-mhard-float", "-Wl,-Map,zig-out/.map", libogc } ++ flags ++ .{ "-o", "zig-out/" ++ options.name ++ ".elf" }));
+    const elf = builder.addSystemCommand(&(.{gcc}));
+    elf.addFileSourceArg(obj.getOutputSource()); // add argument of "{options.name}.o", this file exists somewhere in zig-cache
+    elf.addArgs(&(.{ "-g", "-DGEKKO", "-mrvl", "-mcpu=750", "-meabi", "-mhard-float", "-Wl,-Map,zig-out/.map", libogc } ++ flags ++ .{ "-o", "zig-out/" ++ options.name ++ ".elf" }));
     const dol = builder.addSystemCommand(&.{ elf2dol, "zig-out/" ++ options.name ++ ".elf", "zig-out/" ++ options.name ++ ".dol" });
     builder.default_step.dependOn(&dol.step);
     dol.step.dependOn(&elf.step);
@@ -105,7 +118,7 @@ fn cwd() []const u8 {
 
 // Runs shell command
 fn command(allocator: std.mem.Allocator, dir: []const u8, argv: []const []const u8) !void {
-    var child = try std.ChildProcess.init(argv, allocator);
+    var child = std.ChildProcess.init(argv, allocator);
     child.cwd = dir;
     child.stderr = std.io.getStdErr();
     child.stdout = std.io.getStdOut();
